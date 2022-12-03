42834abbad66a71bdc55ee083e44e35d73e059b2
dkswami <dkswami1820@gmail.com>
2022-12-03 21:16:31 +0530

    auth guard and strategy implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './guards/jwt-strategy';
+import { JwtGuard } from './guards/jwt-guard';
 
 @Module({
 	imports: [UserModule, JwtModule.registerAsync({
@@ -12,6 +14,6 @@ import { AuthService } from './auth.service';
 		})
 	})],
 	controllers: [AuthController],
-	providers: [AuthService]
+	providers: [AuthService, JwtStrategy, JwtGuard]
 })
 export class AuthModule { }
--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseGuards } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { Quiz } from './interfaces/quiz.interface';
 import { CreateQuizDto } from './dto/create-quiz.dto';
+import { JwtGuard } from 'src/auth/guards/jwt-guard';
 
 @Controller('quiz')
 export class QuizController {
 	constructor(private readonly quizService: QuizService) {}
 
+	@UseGuards(JwtGuard)
 	@Get()
 	findAll(): Promise<Quiz[]> {
 		return this.quizService.findAll();
