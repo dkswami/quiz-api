4885edba565846072cf0cdef4183b004cb7cce41
dkswami <dkswami1820@gmail.com>
2022-12-12 15:34:14 +0530

    changes for deployment

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
 	app.setGlobalPrefix('api/v1');
-	await app.listen(4000);
+	const port = process.env.PORT || 4000;
+	await app.listen(port);
 }
 bootstrap();
