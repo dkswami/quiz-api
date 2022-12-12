d6c4a6c5f59b22ba6c85fc5c6af3e4dd61bab041
dkswami <dkswami1820@gmail.com>
2022-12-12 20:53:45 +0530

    package json change node version

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	//app.enableCors();
 	app.setGlobalPrefix('api/v1');
-	const port = process.env.PORT || 3000;
+	const port = process.env.PORT || 4000;
 	await app.listen(port);
 }
 bootstrap();
