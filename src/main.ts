7fd2e435ec5e723a46fdd9c01df97867e961b2c8
dkswami <dkswami1820@gmail.com>
2022-12-12 21:19:09 +0530

    removed api/v1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	//app.enableCors();
-	app.setGlobalPrefix('api/v1');
+	// app.setGlobalPrefix('api/v1');
 	const port = process.env.PORT || 4000;
 	await app.listen(port);
 }
