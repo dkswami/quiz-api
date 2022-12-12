1ade6533d95a3c851535f202e4558702d7f535e2
dkswami <dkswami1820@gmail.com>
2022-12-12 23:33:24 +0530

    added back api/v1

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	//app.enableCors();
-	// app.setGlobalPrefix('api/v1');
+	app.enableCors();
+	app.setGlobalPrefix('api/v1');
 	const port = process.env.PORT || 4000;
 	await app.listen(port);
 }
