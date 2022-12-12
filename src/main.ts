3670503fcb9869ac724b5893451c220ddb096a3a
dkswami <dkswami1820@gmail.com>
2022-12-12 17:13:57 +0530

    port changed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	//app.enableCors();
 	app.setGlobalPrefix('api/v1');
-	const port = process.env.PORT || 4000;
+	const port = process.env.PORT || 3000;
 	await app.listen(port);
 }
 bootstrap();
